ef53217e9d47a168df08a124f68ed3c4728f510c
ogeha <xent978@gmail.com>
2024-11-12 16:28:49 +0300

    1234

--- RGR_Chisl_2/src/Main.java
+++ RGR_Chisl_2/src/Main.java
@@ -8,12 +8,25 @@ public class Main {
         x.add(scanner.nextDouble());
         x.add(scanner.nextDouble());
         x.add(scanner.nextDouble());
-        ArrayList<Double> y =  new ArrayList<Double>();
+        ArrayList<Double> y_h =  new ArrayList<Double>();
+        ArrayList<Double> y_s =  new ArrayList<Double>();
+        double h = (3-1)/2;
+        double s = h/2;
         for (int i = 0; i < 3; i++){
-            y.add(6 * x.get(i) - 4);
+            y_h.add(6 * x.get(i) - 4);
         }
-        int h = (3-1)/2;
-        double Integri = h * ((y.get(0) + y.get(2)) / 2 + y.get(1));
+        for (int i = 0; i < 3; i++){
+            y_s.add(6 * x.get(i) - 4);
+            y_s.add(6 * (x.get(i)+s) - 4);
+        }
+        y_s.remove(5);
+        System.out.println(y_h);
+        System.out.println(y_s);
+        double Integri = h * ((y_h.get(0) + y_h.get(2)) / 2 + y_h.get(1));
         System.out.println(Integri);
+        double Integri2 = s * ((y_s.get(0) + y_s.get(4)) / 2 + y_s.get(1) + y_s.get(2) + y_s.get(3));
+        System.out.println(Integri2);
+        double Runge = (Integri - Integri2)/3;
+        System.out.println(Runge);
     }
 }
